d553530b2098aa5cefb5bcc27eb27046250f89a6
Varun <thevarungupta5@gmail.com>
2024-09-28 12:30:16 +0530

    initial commit

---
+++ CountriesData.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication64.Models.Domain;
+
+namespace WebApplication64
+{
+    public static class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            var countries = new[]
+            {
+                new {  Id =1, Name = "Country 1" },
+                new {  Id =2, Name = "Country 2" },
+                new {  Id =3, Name = "Country 3" },
+                new {  Id =4, Name = "Country 4" },
+                new {  Id =5, Name = "Country 5" },
+                new {  Id =6, Name = "Country 6" },
+                new {  Id =7, Name = "Country 7" },
+                new {  Id =8, Name = "Country 8" },
+            };
+            return countries.Select(c => new Country { Id = c.Id, Name = c.Name }).ToList();
+        }
+    }
+}
---
+++ Models/Domain/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication64.Models.Domain
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
